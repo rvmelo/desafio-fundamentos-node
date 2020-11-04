02d5c5250f1eb8f1c045c419c0cdf260402aafac
rvmelo <robertomelo7@hotmail.com>
2020-11-04 16:42:37 -0300

    challenge finished

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string,
+  value: number,
+  type: 'income' | 'outcome',
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,16 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+
+    const balance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > balance.total) throw new Error('invalid transaction');
+
+    const transaction: Transaction = this.transactionsRepository.create({ title, value, type });
+
+    return transaction;
+
   }
 }
 
